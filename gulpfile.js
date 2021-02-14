9d9035551d8c46ed029824aa107b84ee1da057c5
VladykoD <VladykoD@gmail.com>
2021-02-15 00:26:07 +0300

    save little changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,9 @@ const gulp = require('gulp');
 
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
-const uglify = require('gulp-uglifyes');
 
 const concat = require('gulp-concat');
-const babel = require('gulp-babel');
+
 
 gulp.task('styles', function () {
 	return gulp.src([
@@ -14,30 +13,12 @@ gulp.task('styles', function () {
 		, './css/site.css'
 	])
 		.pipe(concat('style.css'))
-		.pipe(autoprefixer({
-			browsers: ['last 6 versions']
-		}))
+		.pipe(autoprefixer())
 		.pipe(cleanCSS())
 		.pipe(gulp.dest('./css'))
 })
 
-gulp.task('scripts', function () {
-	return gulp.src([
-		  'src/js/select.js'
-		, 'src/js/closest_polyfill.js'
-	])
-		.pipe(babel({
-			presets: ['@babel/preset-env']
-		}))
-		.pipe(uglify({
-			mangle: true,
-			ecma: 6
-		}))
-		.pipe(concat('app.js'))
-		.pipe(gulp.dest('build/js'))
-});
-
 gulp.task('watch', function() {
 	gulp.watch('*/*.css', gulp.series('styles'));
 });
-gulp.task('build', gulp.parallel('styles', 'scripts'));
+gulp.task('build', gulp.parallel('styles'));
